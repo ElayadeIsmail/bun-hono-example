edee5b4532ba7a0b6e860b82ae4b02b78a9cc7f0
Elayade Ismail <elayadeismail@gmail.com>
2024-09-12 19:58:53 +0100

    update config var

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,16 +6,26 @@ const envSchema = z.object({
 		.enum(['development', 'test', 'staging', 'production'])
 		.default('development'),
 	DB_FILE: z.string(),
+	COOKIE_SECRET: z.string(),
+	EXPIRATION_DATE_IN_SEC: z.coerce
+		.number()
+		.int()
+		.positive()
+		.default(30 * 24 * 60 * 60),
 });
 
 const result = envSchema.safeParse(process.env);
 
 if (!result.success) {
-	throw Error(`Invalid Env vars ${result.error.formErrors.fieldErrors}`);
+	throw Error(
+		`Invalid Env vars ${JSON.stringify(result.error.flatten().fieldErrors)}`
+	);
 }
 
 export const envVars = {
 	port: result.data.PORT,
 	env: result.data.ENV,
 	dbFile: result.data.DB_FILE,
+	cookieSession: result.data.COOKIE_SECRET,
+	expirationDateInSec: result.data.EXPIRATION_DATE_IN_SEC,
 };
